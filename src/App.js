6dc9f14f0051d541b98d0daa579058ed6f061633
codewithzaman <naderzaman50@gmail.com>
2024-03-20 01:28:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Cards from "./components/Cards";
+import Category from "./components/Category";
 import Food from "./components/Food";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -10,6 +11,7 @@ function App() {
      <Hero/>
      <Cards/>
      <Food/>
+     <Category/>
     </div>
   );
 }
